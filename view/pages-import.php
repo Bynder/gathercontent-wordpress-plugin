9f5e29e28465f578b027d9b587024cf4db884312
Mathew Chapman <Mathew Chapman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-30 14:26:10 +0000

    Updated to 2.5
    git-svn-id: http://plugins.svn.wordpress.org/gathercontent-import/trunk@999186 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ view/pages-import.php
@@ -0,0 +1,64 @@
+<h2 class="gc_logo"><span>GatherContent</span></h2>
+<?php
+if ( $this->error != '' ){
+	echo '<p class="gc_error">'.$this->error.'</p>';
+}
+?>
+<div class="gc_container gc_wide">
+	<div class="gc_overlay"></div>
+	<div class="gc_container gc_modal gc_importing_modal">
+		<h2><?php $this->_e( 'Importing pages and text content...' ) ?></h2>
+		<label><?php $this->_e( 'Page:' ) ?> <span id="gc_page_title"></span><img src="<?php echo $this->plugin_url ?>img/ajax-loader-grey.gif" alt="" /></label>
+		<div id="current_page" class="progress">
+			<div class="bar" style="width:0%"></div>
+		</div>
+	</div>
+	<div class="gc_container gc_modal gc_repeating_modal">
+		<h2><?php $this->_e( 'Repeating configuration...' ) ?></h2>
+		<img src="<?php echo $this->plugin_url ?>img/ajax_loader_blue.gif" alt="" />
+	</div>
+	<div class="gc_container-header gc_cf">
+		<h2><?php $this->_e( 'Choose pages to import' ) ?><a href="<?php $this->url( 'login' ) ?>" class="gc_blue_link gc_right"><?php $this->_e( 'Choose account' ) ?></a></h2>
+	</div>
+	<form action="<?php $this->url( 'pages_import' ) ?>" method="post" id="gc_importer_step_pages_import">
+		<div class="gc_main_content">
+			<div class="gc_search_pages gc_cf">
+				<div class="gc_left">
+					<a href="<?php $this->url( 'pages' ) ?>" class="gc_option"><?php $this->_e( 'Select different pages' ) ?></a>
+				</div>
+				<div class="gc_right">
+					<?php $page_count > 0 && $this->get_submit_button( $this->__( 'Import selected pages' ) ) ?>
+				</div>
+			</div>
+			<table class="gc_pages" id="gc_pages" cellspacing="0" cellpadding="0">
+				<thead>
+					<tr>
+						<th></th>
+						<th class="gc_th_page_name"><?php echo $this->__( 'Pages' ); ?></th>
+						<th><input type="checkbox" id="toggle_all" checked="checked" /></th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php echo $page_settings ?>
+				</tbody>
+			</table>
+		</div>
+		<div class="gc_subfooter gc_cf">
+			<div class="gc_right">
+				<?php $page_count > 0 && $this->get_submit_button( $this->__( 'Import selected pages' ) ) ?>
+			</div>
+		</div>
+		<?php wp_nonce_field( $this->base_name ) ?>
+	</form>
+</div>
+<script type="text/javascript">
+var redirect_url = <?php
+echo json_encode(
+	array(
+		'media'        => $this->url( 'media', false ),
+		'finished'     => $this->url( 'finished', false )
+	)
+);
+?>;
+var hierarchical_post_types =  <?php echo json_encode( $this->hierarchical ); ?>;
+</script>
