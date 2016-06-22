8751813c5a8d8732f3171f2b6683391a61a6b150
Justin Sternberg <justin@dsgnwrks.pro>
2016-06-22 10:06:22 -0400

    Clean up styling a bit

--- includes/views/tmpl-gc-items-sync.php
+++ includes/views/tmpl-gc-items-sync.php
@@ -1,5 +1,5 @@
 <legend class="screen-reader-text"><?php _e( 'Sync Items', 'gathercontent-import' ); ?></legend>
-<table class="widefat">
+<table class="widefat striped">
 	<thead>
 		<tr>
 			<th id="cb" class="gc-field-th manage-column column-cb check-column"><label class="screen-reader-text" for="cb-select-all-1"><?php _e( 'Select All', 'gathercontent-import' ); ?></label><input id="cb-select-all-1" type="checkbox"></th>
@@ -11,4 +11,12 @@
 	<tbody>
 		<tr><td colspan="3"><span class="gc-loader spinner is-active"></span></td></tr>
 	</tbody>
+	<tfoot>
+		<tr>
+			<th id="cb" class="gc-field-th manage-column column-cb check-column"><label class="screen-reader-text" for="cb-select-all-1"><?php _e( 'Select All', 'gathercontent-import' ); ?></label><input id="cb-select-all-1" type="checkbox"></th>
+			<th class="gc-field-th'"><?php _e( 'Item', 'gathercontent-import' ); ?></th>
+			<th class="gc-field-th'"><?php _e( 'Status', 'gathercontent-import' ); ?></th>
+			<th id="gc-edit" class="gc-field-th'"><?php _e( 'Edit', 'gathercontent-import' ); ?></th>
+		</tr>
+	</tfoot>
 </table>
