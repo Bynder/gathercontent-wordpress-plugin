ea831b9beefedebd479ec395b786553872962094
Justin Sternberg <justin@dsgnwrks.pro>
2016-07-12 11:01:41 -0400

    Clean up/remove old functionality

--- assets/js/src/components/collections/tab-rows.js
+++ assets/js/src/components/collections/tab-rows.js
@@ -4,17 +4,6 @@ module.exports = function( app ) {
 
 		initialize: function( models, options ) {
 			this.tab = options.tab;
-		},
-
-		showTab: function( id ) {
-			var model = this.getById( id );
-
-			if ( model ) {
-				this.invoke( 'set', { 'hidden': true } );
-				model.set( 'hidden', false );
-				this.trigger( 'render' );
-			}
 		}
-
 	});
 };
