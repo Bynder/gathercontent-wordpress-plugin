669e4563cef2d9ebb30cf3500e89e7c2c5e7e05b
shehrozsheikh <shehrozsheikh@outlook.com>
2021-11-23 02:26:05 +0500

    Added minimum PHP version

--- gathercontent-importer.php
+++ gathercontent-importer.php
@@ -1,14 +1,15 @@
 <?php
 /**
- * Plugin Name: GatherContent Plugin
- * Plugin URI:  http://www.gathercontent.com
- * Description: Imports items from GatherContent to your wordpress site
- * Version:     3.2.0
- * Author:      GatherContent
- * Author URI:  http://www.gathercontent.com
- * Text Domain: gathercontent-import
- * Domain Path: /languages
- * License:     GPL-2.0+
+ * Plugin Name:  GatherContent Plugin
+ * Plugin URI:   http://www.gathercontent.com
+ * Description:  Imports items from GatherContent to your wordpress site
+ * Version:      3.2.0
+ * Author:       GatherContent
+ * Requires PHP: 7.0
+ * Author URI:   http://www.gathercontent.com
+ * Text Domain:  gathercontent-import
+ * Domain Path:  /languages
+ * License:      GPL-2.0+
  */
 
 /**
