0b753bd487a0cecda122170c3a3eea2b28aa3f64
Hugh Downer <hugh.downer@gmail.com>
2022-05-03 20:20:55 +0100

    Added ralouphie/mimey to dependencies

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,4 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Mimey\\' => array($vendorDir . '/ralouphie/mimey/src'),
 );
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -19,6 +19,15 @@
             'reference' => NULL,
             'dev_requirement' => false,
         ),
+        'ralouphie/mimey' => array(
+            'pretty_version' => '1.0.2',
+            'version' => '1.0.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../ralouphie/mimey',
+            'aliases' => array(),
+            'reference' => '2a0e997c733b7c2f9f8b61cafb006fd5fb9fa15a',
+            'dev_requirement' => false,
+        ),
         'techcrunch/wp-async-task' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
