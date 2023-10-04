35cc64879dd3758b9df89f04d61a59daaa30d997
szydlofj <caleb.szydlo@fjorgedigital.com>
2023-10-04 16:03:22 -0500

    cleaning up for pr

--- includes/classes/admin/mapping/field-types/wpseo.php
+++ includes/classes/admin/mapping/field-types/wpseo.php
@@ -127,6 +127,10 @@ class WPSEO extends Base implements Type {
 		new \WPSEO_Metabox();
 		\WPSEO_Metabox::translate_meta_boxes();
 
+		if ( $options['opengraph'] === true || $options['twitter'] === true || $options['googleplus'] === true ) {
+			new \WPSEO_Social_Admin();
+			\WPSEO_Social_Admin::translate_meta_boxes();
+		}
 	}
 
 	public function remove_wpseo_keys( $blacklist ) {
