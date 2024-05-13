6f21103795bff6d5ebf8711e5ccf542a9e5d586b
Gavin Allmond <gavin.allmond@bynder.com>
2024-05-13 16:41:18 +0100

    Update database dropdown to use dynamically updated hidden input

--- includes/classes/admin/mapping/field-types/database.php
+++ includes/classes/admin/mapping/field-types/database.php
@@ -61,7 +61,28 @@ class Database extends Base implements Type {
 
 		$subValueName = $view->get( 'option_base' ) . '[mapping][{{ data.name }}][sub-value]';
 
-		//TODO Gavin this cannot be the done way of adding on-page js. Do it properly.
+		/**
+		 * TODO Gavin: This cannot be the 'proper' WP way of doing dynamic select
+		 * options it his horrendous. Do it properly
+		 */
+		$tableSelectOnChangeJavascript = <<<EOT
+/** this runs when the table selector is changed */
+const selectElement = this
+const value = selectElement.value
+
+// set this value as the second portion of the hidden element's value
+const hidden = document.getElementById('hidden-database-table-name')
+let hiddenVal = hidden.value
+if(!hiddenVal.includes('.')){
+	hiddenVal = '.'
+}
+const parts = hiddenVal.split('.')
+parts.splice(1, 1, value)
+hidden.value = parts.join('.')
+EOT;
+
+
+
 		$mainSelectOnChangeJavascript = <<<EOT
 /** @type {HTMLSelectElement} selectElement */
 const selectElement = this
@@ -77,22 +98,47 @@ subSelectElement.value = ''
 // hide any option whose data-tablename is not this text
 subSelectElement.querySelectorAll('option').forEach(opt => {
 	const optTableName = opt.getAttribute('data-tablename')
-
 	opt.style.display = optTableName === text ? 'block' : 'none'
 })
+
+// set this value as the first portion of the hidden element's value
+const hidden = document.getElementById('hidden-database-table-name')
+let hiddenVal = hidden.value
+if(!hiddenVal.includes('.')){
+	hiddenVal = '.'
+}
+const parts = hiddenVal.split('.')
+parts.splice(0, 1, value)
+hidden.value = parts.join('.')
 EOT;
 
 		?>
 		<# if ( '<?php $this->e_type_id(); ?>' === data.field_type ) { #>
-			<select onchange="<?= $mainSelectOnChangeJavascript ?>" class="gc-select2 wp-type-value-select <?php $this->e_type_id(); ?>" name="<?php $view->output( 'option_base' ); ?>[mapping][{{ data.name }}][value]">
+			<select
+				onchange="<?= $mainSelectOnChangeJavascript ?>"
+				class="gc-select2 wp-type-value-select <?php $this->e_type_id(); ?>"
+				name=""
+			>
 				<?php $this->underscore_options( $this->post_options ); ?>
 				<?php $this->underscore_empty_option( __( 'Do Not Import', 'gathercontent-import' ) ); ?>
 			</select>
-<!--		//TODO Gavin how do we pass the selection down on submit?-->
-			<select name="<?= $subValueName ?>" class="cw-column-selector">
+			<select
+				onchange="<?= $tableSelectOnChangeJavascript ?>"
+				name=""
+				class="cw-column-selector">
 				<option value="">Select a column</option>
 				<?= implode('\r\n', $this->getAllTableColOptions()) ?>
 			</select>
+
+<!--		//TODO gavin - this is the actual input that is passed on form submit -->
+<!--		//TODO gavin - how do we use this to pre-fill the drop-downs? -->
+			<input
+				id="hidden-database-table-name"
+				type="hidden"
+				name="<?php $view->output( 'option_base' ); ?>[mapping][{{ data.name }}][value]"
+				value=""
+			/>
+
 		<# } #>
 		<?php
 	}
